314631d693572fc48f5b2885a4e174e21d4c9350
crowphy <672570250@qq.com>
2015-04-24 00:09:14 +0800

    Update resume.js

--- js/resume.js
+++ js/resume.js
@@ -134,57 +134,5 @@ window.onload=function(){
 		}
 
 	}
-
-	function drawWatch(){
-
-        cxt.clearRect(-w / 2,- h / 2,w,h);
-        var len=Math.min(w,h)/2;
-        var len1=0.85*len;
-        cxt.font="30px Georgia";
-        cxt.fillStyle="black";
-        cxt.textAlign="center";
-        cxt.textBaseline="middle";
-        for(var i=0;i<12;i++){
-            var tag1=Math.PI*2*(3-i)/12;
-            var tx=len*Math.cos(tag1);
-            var ty=-len*Math.sin(tag1);
-            cxt.fillText(i,tx,ty);
-        }
-        var d=new Date();
-        var h=d.getHours();
-        var m=d.getMinutes();
-        var s=d.getSeconds();
-        if(h>12)h=h-12;
-        var hAngle=Math.PI*2*(3-(h+m/60))/12;
-        var hLen=0.5*len;
-        var hWidth=4;
-        var hColor="black";
-        drawHand(cxt,hAngle,hLen,hWidth,hColor);
-
-        var mAngle=Math.PI*2*(15-(m+s/60))/60;
-        var mLen=0.7*len;
-        var mWidth=3;
-        var mColor="black";
-        drawHand(cxt,mAngle,mLen,mWidth,mColor);
-
-        var sAngle=Math.PI*2*(15-s)/60;
-        var sLen=0.8*len;
-        var sWidth=1;
-        var sColor="#CC0033";
-        drawHand(cxt,sAngle,sLen,sWidth,sColor);
-
-        setTimeout(drawWatch,1000);
-    }
-
-    function drawHand(cxt,angle,length,width,color){
-        var x=length*Math.cos(angle);
-        var y=-length*Math.sin(angle);
-        cxt.strokeStyle=color;
-        cxt.lineWidth=width;
-        cxt.lineCap="round";
-        cxt.beginPath();
-        cxt.moveTo(0,0);
-        cxt.lineTo(x,y);
-        cxt.stroke();
-    }		
+		
 }
